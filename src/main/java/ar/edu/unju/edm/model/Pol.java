3992576a28373d86945d3cf61b96eba2854fedef
Zoe Marcial <Zoe Marcial@LAPTOP-QPF45C5K>
2021-05-29 20:47:58 -0300

    agregado atributos a la clase pol selene zoe aylen marcial

--- src/main/java/ar/edu/unju/edm/model/Pol.java
+++ src/main/java/ar/edu/unju/edm/model/Pol.java
@@ -1,5 +1,121 @@
 package ar.edu.unju.edm.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table (name="POLS")
+@Component
 public class Pol {
 
+	@Id
+	@Column
+	private int codigoPol;
+	
+	@Column
+	private String nombre;
+	@Column
+	private String descripcion;
+	@Column
+	private String etiqueta;
+	@Column
+	private String sitioWeb;
+	@Column
+	private String calle;
+	@Column
+	private int numeroCasa;
+	@Column
+	private String barrio;
+	@Column
+	private String localidad;
+	@Column
+	private int latitud;
+	@Column
+	private int longitud;
+	@Column
+	private String email;
+	
+	public Pol() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public int getCodigoPol() {
+		return codigoPol;
+	}
+	public void setCodigoPol(int codigoPol) {
+		this.codigoPol = codigoPol;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getEtiqueta() {
+		return etiqueta;
+	}
+	public void setEtiqueta(String etiqueta) {
+		this.etiqueta = etiqueta;
+	}
+	public String getSitioWeb() {
+		return sitioWeb;
+	}
+	public void setSitioWeb(String sitioWeb) {
+		this.sitioWeb = sitioWeb;
+	}
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public int getNumeroCasa() {
+		return numeroCasa;
+	}
+	public void setNumeroCasa(int numeroCasa) {
+		this.numeroCasa = numeroCasa;
+	}
+	public String getBarrio() {
+		return barrio;
+	}
+	public void setBarrio(String barrio) {
+		this.barrio = barrio;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public int getLatitud() {
+		return latitud;
+	}
+	public void setLatitud(int latitud) {
+		this.latitud = latitud;
+	}
+	public int getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 }
