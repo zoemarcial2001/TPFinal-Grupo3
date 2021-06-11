f8db25e0599943c8f43347064a28a2755cab38f9
Zoe Marcial <Zoe Marcial@LAPTOP-QPF45C5K>
2021-06-11 15:10:50 -0300

    agregados vistas verpoi, turistaregistro modificaciones en las vistas valoracion y otros
    selene zoe aylen marcial

--- src/main/java/ar/edu/unju/edm/controller/HomeController.java
+++ src/main/java/ar/edu/unju/edm/controller/HomeController.java
@@ -16,5 +16,5 @@ public class HomeController {
 	public String cargarInicio(Model model){	
 	return "inicio";
 	}
-}
+	}
 
--- src/main/java/ar/edu/unju/edm/repository/IPoIDAO.java
+++ src/main/java/ar/edu/unju/edm/repository/IPoIDAO.java
@@ -1,12 +1,14 @@
 package ar.edu.unju.edm.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import ar.edu.unju.edm.model.PoI;
+import ar.edu.unju.edm.model.Turista;
 
 
 @Repository
@@ -14,4 +16,6 @@ public interface IPoIDAO extends CrudRepository<PoI, Integer>{
 
 	@Query("from PoI p order by p.codigoPoI")
 	public List<PoI> obtenerPoIs();
+	
+	public Optional<PoI> findAllByTuristaAutor(Turista turistaAutor);
 }
